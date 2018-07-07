aefe0379ee43ba31f3020fa4569a9c27dfac5a02
BILLPC2684 <BILLPC2684@gmail.com>
2018-07-07 14:30:26 -0400

    quick fix

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
-reset
+[[ "$#" -gt 0 ]] && reset
 rm --force ./TGR-C
 gcc main.c -o ./TGR-C -std=c99 -lm `pkg-config --cflags --libs sdl2` -Wall
-if [ "$#" -gt 0 ]; then
- ./TGR-C $1 $2 $3 $4 $5 $6 $7 $8 $9
-fi
+[[ "$#" -gt 0 ]] && ./TGR-C $1 $2 $3 $4 $5 $6 $7 $8 $9
